827698638a6249185ce0e707e0f3625fe4c118c8
1CarlosRenan <carlosrenan96@gmail.com>
2022-08-08 22:12:11 -0300

    Consumindo API com métodos GET e POST utilizando formulário no front

---
+++ 3-app-animal2/src/server.py
@@ -0,0 +1,63 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from typing import List, Optional
+from pydantic import BaseModel
+from uuid import uuid4, UUID
+
+app = FastAPI()
+
+origins = ['http://localhost:5500']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+class Animal(BaseModel):
+    id: Optional[str]
+    nome: str
+    idade: int
+    sexo: str
+    cor: str
+
+
+banco: List[Animal] = []
+
+
+@app.get('/animais')
+def listar_animais():
+    return banco
+
+
+@app.get('/animais/{animal_id}')
+def obter_animal(animal_id: UUID):
+    for animal in banco:
+        if animal.id == animal_id:
+            return animal
+    return {'error': 'Animal não localizado'}
+
+
+@app.delete('/animais/{animal_id}')
+def remover_animal(animal_id: str):
+    posicao = -1
+    for index, animal in enumerate(banco):
+        if animal.id == animal_id:
+            posicao = index
+            break
+
+    if posicao != -1:
+        banco.pop(posicao)
+        return {'mensagem': 'Animal removido com sucesso'}
+    else:
+        return {'error': 'Animal não localizado'}
+
+
+@app.post('/animais')
+def criar_animal(animal: Animal):
+    animal.id = uuid4()
+    banco.append(animal)
+    return None
