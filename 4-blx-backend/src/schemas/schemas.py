0d811776433781a4a68b004309e8542c43f7bc3a
1CarlosRenan <carlosrenan96@gmail.com>
2022-08-19 20:11:58 -0300

    Incrementando o ORM

--- 4-blx-backend/src/schemas/schemas.py
+++ 4-blx-backend/src/schemas/schemas.py
@@ -6,14 +6,13 @@ class Usuario(BaseModel):
     id: Optional[str] = None
     nome: str
     telefone: str
-    meus_produtos: List[Pedido]
+    meus_produtos: List[Produto]
     minhas_vendas: List[Pedido]
     meus_pedidos: List[Pedido]
 
 
 class Produto(BaseModel):
     id: Optional[str] = None
-    usuario: Usuario
     nome: str
     detalhes: str
     preco: float
