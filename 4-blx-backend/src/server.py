65bbd0dcf9685a2650f600b6d3b83d9663db3236
1CarlosRenan <carlosrenan96@gmail.com>
2022-10-16 15:01:38 -0300

    Incrementando HTTP Status Code

--- 4-blx-backend/src/server.py
+++ 4-blx-backend/src/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status
 from sqlalchemy.orm import Session
 from src.infra.sqlalchemy.config.database import get_db, criar_db
 from src.schemas import schemas
@@ -9,13 +9,13 @@ criar_db()
 app = FastAPI()
 
 
-@app.post('/produtos')
+@app.post('/produtos', status_code=status.HTTP_201_CREATED)
 def criar_produto(produto: schemas.Produto, db: Session = Depends(get_db)):
     produto_criado = RepositorioProduto(db).criar(produto)
     return produto_criado
 
 
-@app.get('/produtos')
+@app.get('/produtos', status_code=status.HTTP_200_OK)
 def listar_produtos(db: Session = Depends(get_db)):
     produtos = RepositorioProduto(db).listar()
     return produtos
