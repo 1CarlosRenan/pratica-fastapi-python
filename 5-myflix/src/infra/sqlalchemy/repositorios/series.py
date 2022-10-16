b92c8cb9abb7476204b4cddd3b10c13e06d56281
1CarlosRenan <carlosrenan96@gmail.com>
2022-10-15 21:54:32 -0300

    Incrementando o repositorio e o server

---
+++ 5-myflix/src/infra/sqlalchemy/repositorios/series.py
@@ -0,0 +1,30 @@
+from sqlalchemy.orm import Session
+from src.infra.sqlalchemy.models import models
+from src.schemas import schemas
+
+
+class RepositorioSerie():
+
+    def __init__(self, db: Session):
+        self.db = db
+
+    def criar(self, serie: schemas.Serie):
+        db_serie = models.Serie(titulo=serie.titulo,
+                                ano=serie.ano,
+                                genero=serie.genero,
+                                qtd_temporadas=serie.qtd_temporadas
+                                )
+        self.db.add(db_serie)
+        self.db.commit()
+        self.db.refresh(db_serie)
+
+        return db_serie
+
+    def listar(self):
+        series = self.db.query(models.Serie).all()
+
+    def obter(self):
+        pass
+
+    def remover(self):
+        pass
---
+++ 5-myflix/src/server.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends
+from src.infra.sqlalchemy.config.database import criar_db, get_db
+from src.schemas import schemas
+from src.infra.sqlalchemy.repositorios.series import RepositorioSerie
+
+# Criar a base de dados
+criar_db()
+
+app = FastAPI()
+
+
+@app.post('/series')
+def criar_serie(serie: schemas.Serie, db: Session = Depends(get_db)):
+    serie_criada = RepositorioSerie(db).criar(serie)
+    return serie_criada
+
+
+@app.get('/series')
+def listar_serie(db: Session = Depends(get_db)):
+    return RepositorioSerie(db).listar()
