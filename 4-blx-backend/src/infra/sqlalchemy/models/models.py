47b5ecb4a2715140b424707116a3fc1190e85822
1CarlosRenan <carlosrenan96@gmail.com>
2022-10-17 20:28:56 -0300

    Incrementando modelo Usuario e Relacionamentos

--- 4-blx-backend/src/infra/sqlalchemy/models/models.py
+++ 4-blx-backend/src/infra/sqlalchemy/models/models.py
@@ -1,5 +1,17 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 from src.infra.sqlalchemy.config.database import Base
+
+
+class Usuario(Base):
+    __tablename__ = 'usuario'
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String)
+    senha = Column(String)
+    telefone = Column(String)
+
+    produtos = relationship('Produto', back_populates='usuario')
 
 
 class Produto(Base):
@@ -11,3 +23,6 @@ class Produto(Base):
     preco = Column(Float)
     disponivel = Column(Boolean)
     tamanhos = Column(String)
+    usuario_id = Column(Integer, ForeignKey('usuario.id'))
+
+    usuario = relationship('Usuario', back_populates='produtos')
