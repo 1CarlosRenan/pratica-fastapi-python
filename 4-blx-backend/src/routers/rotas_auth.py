98b1202e1684b81db55a67de80b965339a6926a2
1CarlosRenan <carlosrenan96@gmail.com>
2022-11-04 22:42:44 -0300

    fix: renomeando rotas_user

--- 4-blx-backend/src/routers/rotas_auth.py
+++ 4-blx-backend/src/routers/rotas_auth.py
@@ -3,19 +3,15 @@ from fastapi import APIRouter, status, Depends
 from sqlalchemy.orm import Session
 from src.infra.sqlalchemy.config.database import get_db
 from src.infra.sqlalchemy.repositorios.repositorio_usuario import RepositorioUsuario
-from src.schemas.schemas import Usuario
+from src.schemas.schemas import Usuario, UsuarioSimples
+from src.infra.providers import hash_provider
 
 
 router = APIRouter()
 
 
-@router.post('/signup', status_code=status.HTTP_201_CREATED, response_model=Usuario)
+@router.post('/signup', status_code=status.HTTP_201_CREATED, response_model=UsuarioSimples)
 def signup(usuario: Usuario, session: Session = Depends(get_db)):
+    usuario.senha = hash_provider.gerar_hash(usuario.senha)
     usuario_criado = RepositorioUsuario(session).criar(usuario)
     return usuario_criado
-
-
-@router.get('/usuarios', response_model=List[Usuario])
-def listar_usuario(session: Session = Depends(get_db)):
-    usuarios = RepositorioUsuario(session).listar()
-    return usuarios
